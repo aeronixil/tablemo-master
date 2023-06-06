1bc7627aed7b9a1f4948406c31bc6196f780e82d
aeronixil <kingvenkat07@gmail.com>
2023-06-07 00:02:30 +0530

    init git

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:ar_home/views/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'radAR',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme:
+              GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              iconTheme: const IconThemeData(color: Colors.black),
+              titleTextStyle:
+                  GoogleFonts.nunito(color: Colors.black, fontSize: 20))),
+      home: const MyHomePage(title: 'AR Home'),
+    );
+  }
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,26 @@
+import 'package:ar_home/views/ar_viewer.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({required this.title, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("AR Home"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LocalAndWebObjectsView()));
+              },
+              child: const Text("Start")),
+        ));
+  }
+}
